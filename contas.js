9aa5e4c86a90686db1e8eb3bf5d28a52ed4a7d53
PedroFatec <pedro.fatec@fatec.sp.gov.br>
2023-08-21 22:05:32 -0300

    Modificações de testes

--- contas.js
+++ contas.js
@@ -1,29 +1,22 @@
-function soma(a,b)
-{
-
-    return a+b;
-
-}
-
-function subtracao(a,b)
-{
-
-    return a-b;
-
-}
-
-function multiplicacao(a,b)
-{
-
-    return a*b;
-
-}
-
-function divisao(a,b)
-{
-
-    return a/b;
-
-}
-
-export{soma, subtracao, multiplicacao, divisao}
+function soma(a, b) {
+    return a + b;
+  }
+  
+  function subtracao(a, b) {
+    return a - b;
+  }
+  
+  function multiplicacao(a, b) {
+    return a * b;
+  }
+  
+  function divisao(a, b) {
+    return a / b;
+  }
+  
+  module.exports = {
+    soma,
+    subtracao,
+    multiplicacao,
+    divisao,
+  };
--- contas.test.js
+++ contas.test.js
@@ -1,7 +1,17 @@
-const contas = require('./contas');
+const contas = require("./contas");
 
-test("Soma 2+5 esperado 7 como resultado",()=>{
+test("Soma 2+5 esperado 7 como resultado", () => {
+  expect(contas.soma(2, 5)).toBe(7);
+});
 
-    expect(contas.soma(2, 5)).toBe(7);
+test("Subtração 10-5 esperado 5 como resultado", () => {
+  expect(contas.subtracao(10, 5)).toBe(5);
+});
 
-})
+test("Multiplicação 10*5 esperado 50 como resultado", () => {
+  expect(contas.multiplicacao(10, 5)).toBe(50);
+});
+
+test("Divisão 10/5 esperado 2 como resultado", () => {
+  expect(contas.divisao(10, 5)).toBe(2);
+});
